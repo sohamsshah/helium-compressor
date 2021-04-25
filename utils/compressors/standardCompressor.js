7769b75090ba9488b0f990d581156a6c7c00ef2d
sohamsshah <sohamshah456@gmail.com>
2021-04-25 18:06:13 +0530

    update compressor

--- utils/compressors/standardCompressor.js
+++ utils/compressors/standardCompressor.js
@@ -15,49 +15,38 @@ const heliumZip = (string) => {
   
     return res;
   }
+
   
-  const freqDict = (stringArr) => {
-    const state = {frequency:0, curr_digit:"", prev:stringArr[0]}
-    let res = {}
-    for(let char in stringArr){
-      if(!isNaN(parseInt(stringArr[char]))){
-        state.curr_digit+=stringArr[char]
-      }else{
-        if(state.curr_digit.length === 0){
-          state.frequency = 1
-        }else{
-          state.frequency = parseInt(state.curr_digit)
-        }
-        res[state.prev] = state.frequency
-  
-        state.frequency = 0
-        state.curr_digit = ""
-        state.prev = stringArr[char]
-      }
+const heliumUnZip = (string) => {
+  const stringArr = string.split("");
+  const state = {curr_digit:"", prev:""}
+  let freq = {}
+  let res = []
+  for(let char in stringArr){
+    if(!(isNaN(parseInt(stringArr[char])))){
+      state.curr_digit+=stringArr[char]
       
-    }
-    if(state.curr_digit.length === 0){
-          state.frequency = 1
-      }else{
-        state.frequency = parseInt(state.curr_digit)
+    }else{
+      if(state.prev.length !== 0){
+        if(state.curr_digit.length !== 0){
+          freq[state.prev] = parseInt(state.curr_digit)
+        }
+        res = [...res, ...Array(freq[state.prev]).fill(state.prev)]
+        freq = {}
+        state.curr_digit = ""
       }
-    res[state.prev] = state.frequency
-    return res
+      state.prev = stringArr[char]
+      freq[stringArr[char]] = 1 
+    }
     
   }
-  
-  const heliumUnZip = (string) => {
-    const stringArr = string.split("");
-    const frequencies = freqDict(stringArr);
-    let res = []
-    for(char in frequencies){
-      res = [...res, ...Array(frequencies[char]).fill(char)]
-    }
-    return res.join("")
-  
+  if(state.curr_digit.length === 0){
+    res = [...res, state.prev]
+  }else{
+    res = [...res, ...Array(parseInt(state.curr_digit)).fill(state.prev)]
   }
+  return res.join("")
+}
    
-  // heliumZip("aabbccddessssssssssssssssssssssssssssss");
-  heliumUnZip("a1b1c1d20");
   
-  module.exports = {heliumUnZip, heliumZip}
+module.exports = {heliumUnZip, heliumZip}
