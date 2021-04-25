3d91267f69a2d04e5c2c57cc08684d1d57a666a1
sohamsshah <sohamshah456@gmail.com>
2021-04-25 17:04:13 +0530

    update unzip algo

--- utils/compressors/standardCompressor.js
+++ utils/compressors/standardCompressor.js
@@ -48,16 +48,33 @@ const heliumZip = (string) => {
   
   const heliumUnZip = (string) => {
     const stringArr = string.split("");
-    const frequencies = freqDict(stringArr);
+    const state = {curr_digit:"", prev:""}
+    let freq = {}
     let res = []
-    for(char in frequencies){
-      res = [...res, ...Array(frequencies[char]).fill(char)]
+    for(let char in stringArr){
+      if(!(isNaN(parseInt(stringArr[char])))){
+        state.curr_digit+=stringArr[char]
+        
+      }else{
+        if(state.prev.length !== 0){
+          if(state.curr_digit.length !== 0){
+            freq[state.prev] = parseInt(state.curr_digit)
+          }
+          res = [...res, ...Array(freq[state.prev]).fill(state.prev)]
+          freq = {}
+          state.curr_digit = ""
+        }
+        state.prev = stringArr[char]
+        freq[stringArr[char]] = 1 
+      }
+      
+    }
+    if(state.curr_digit.length === 0){
+      res = [...res, state.prev]
+    }else{
+      res = [...res, ...Array(parseInt(state.curr_digit)).fill(state.prev)]
     }
     return res.join("")
-  
   }
-   
-  // heliumZip("aabbccddessssssssssssssssssssssssssssss");
-  heliumUnZip("a1b1c1d20");
   
   module.exports = {heliumUnZip, heliumZip}
