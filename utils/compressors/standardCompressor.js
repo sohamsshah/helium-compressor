f7d7277e210c2dbb6b5247bbf69ec4f480f292bf
sohamsshah <sohamshah456@gmail.com>
2021-04-25 18:10:02 +0530

    fix conflicts

--- utils/compressors/standardCompressor.js
+++ utils/compressors/standardCompressor.js
@@ -14,8 +14,7 @@ const heliumZip = (string) => {
     res = res = [...res, `${stringArr[parseInt(char)]}${(state.curr_count > 1)?(state.curr_count):""}`].join("")
   
     return res;
-  }
-
+}
   
 const heliumUnZip = (string) => {
   const stringArr = string.split("");
@@ -40,37 +39,12 @@ const heliumUnZip = (string) => {
     }
     
   }
-  
-  const heliumUnZip = (string) => {
-    const stringArr = string.split("");
-    const state = {curr_digit:"", prev:""}
-    let freq = {}
-    let res = []
-    for(let char in stringArr){
-      if(!(isNaN(parseInt(stringArr[char])))){
-        state.curr_digit+=stringArr[char]
-        
-      }else{
-        if(state.prev.length !== 0){
-          if(state.curr_digit.length !== 0){
-            freq[state.prev] = parseInt(state.curr_digit)
-          }
-          res = [...res, ...Array(freq[state.prev]).fill(state.prev)]
-          freq = {}
-          state.curr_digit = ""
-        }
-        state.prev = stringArr[char]
-        freq[stringArr[char]] = 1 
-      }
-      
-    }
-    if(state.curr_digit.length === 0){
-      res = [...res, state.prev]
-    }else{
-      res = [...res, ...Array(parseInt(state.curr_digit)).fill(state.prev)]
-    }
-    return res.join("")
+  if(state.curr_digit.length === 0){
+    res = [...res, state.prev]
+  }else{
+    res = [...res, ...Array(parseInt(state.curr_digit)).fill(state.prev)]
   }
+  return res.join("")
 }
   
 module.exports = {heliumUnZip, heliumZip}
